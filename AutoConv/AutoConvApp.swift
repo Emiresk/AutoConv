46be202b640c85d02794de2e35f42a59719cc21c
Emiresk <svv.luga@gmail.com>
2022-12-29 18:44:00 +0200

    Initial Commit

---
+++ AutoConv/AutoConvApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoConvApp.swift
+//  AutoConv
+//
+//  Created by Vitaliy on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AutoConvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
